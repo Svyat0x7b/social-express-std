c8c76437316ad88958e135cdf5fdcf3c976611eb
Svyat <tokak3845@gmail.com>
2024-01-09 20:12:07 +0200

    fixed port issue bugfix1

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const format = require('date-format');
 
 const app = express();
-const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-    res.status(200).send('<h1>Hello!</h1>');
+    res.status(200).send('<h1>Hello1!</h1>');
 });
 app.get('/api/v1/instagram', (req, res) => {
     const social = {
